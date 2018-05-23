8c8f2d4d8391b720a754b7343736eae52581c94e
ye fan <yefan@startdt.com>
2018-05-23 17:17:30 +0800

    add annoation

---
+++ src/main/java/com/yefan/concurrency/annoations/NoThreadSafe.java
@@ -0,0 +1,15 @@
+package com.yefan.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *  课程中用来标记【线程不安全】类或者方法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NoThreadSafe {
+    String value() default "";
+}
